2e3f3da840559fd9a005ad528b09074341b78566
Eduardo Cabral <theduardds@gmail.com>
2018-03-12 20:02:13 +0000

    update dburl

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ var express         = require("express"),
 var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
-    
+
 // seedDB();
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://theduardds:03073648@ds213239.mlab.com:13239/theduardds_yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("mongodb://theduardds:03073648@ds213239.mlab.com:13239/theduardds_yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
